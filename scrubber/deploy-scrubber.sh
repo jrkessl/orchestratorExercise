164b9fdb0400c018d4f5d6c16e93213717cf6908
Juliano Kessler <juliano.kessler@viapath.com>
2023-05-15 08:39:29 -0300

    deploy scrubber enhanced,now looks at all namespaces and all repositories at once

--- scrubber/deploy-scrubber.sh
+++ scrubber/deploy-scrubber.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
-# this checks which deployments do NOT have dkr.gtl.net/dkr-release in their image (they have dkr.gtl.net/dkr but they do not have dkr.gtl.net/dkr-release).
+# this checks which deployments are not using any of these repositories in their images:
+#   dkr.gtl.net/dkr-release
+#   dkr.gtl.net/dkr-qa
+#   dkr.gtl.net/dkr-production
+# instead, they are simply using dkr.gtl.net/dkr/*. 
 echo "Checking deployments..."
-namespace=release
-for dep in $( kubectl get deploy -n $namespace | grep -v NAME | awk '{print $1}' ); do
-    image=$(kubectl -n $namespace get deploy/$dep -o yaml | grep -i image)
-    match=$(echo $image | grep dkr.gtl.net/dkr | wc -l)
-    if [[ $match -gt 0 ]]; then
-        match2=$(echo $image | grep -v dkr-release | wc -l)
-        if [[ $match2 -gt 0 ]]; then
-            echo "MATCH: $dep $image"
+#namespace=release
+for names in $( kubectl get ns | grep -v NAME | awk '{print $1}' ); do
+    echo "namespace = $names"
+    for dep in $( kubectl get deploy -n $names | grep -v NAME | awk '{print $1}' ); do
+        image=$(kubectl -n $names get deploy/$dep -o yaml | grep -i image: )
+        image=$(echo $image | xargs)
+        match_dkr=$(echo $image | grep dkr.gtl.net/dkr | wc -l)
+        if [[ $match_dkr -gt 0 ]]; then
+            match_nonbasic_repo=$(echo $image | grep -E -- "dkr.gtl.net/dkr[^/]" | wc -l)
+            if [[ $match_nonbasic_repo -eq 0 ]]; then
+                printf "MATCH: $names/%-40s$image," "$dep"
+                echo ""
+            fi
+        else
+            printf "no match: $names/%-40s$image," "$dep"
+            echo ""
         fi
-    else
-        echo "no match: $dep $image"
-    fi
+    done
 done
 echo "Done."
 echo ""
 exit 0
-
-crb
-rb
-
-
-
-for pod in $(kubectl get pods -n qa |grep snap-rails|awk '{print $1}'); do v=$(kubectl exec -n qa $pod -it bash -- cat version); echo "$pod\t$v"; done <-- FOR NERVOSO
