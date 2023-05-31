af0e28ac91a39af0d6caf33ee79c3dc6a989b14c
Juliano Kessler <juliano.kessler@viapath.com>
2023-05-31 16:09:59 -0300

    pod scrubber, finalized

--- scrubber/pod-scrubber-var.sh
+++ scrubber/pod-scrubber-var.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-# scrubs all pods looking for a variable
+# scrubs all pods looking for the value of variable WAIT
 echo "Checking pods..."
-#namespace=qa
-#for names in $( kubectl get ns | grep -v NAME | awk '{print $1}' ); do
-#    echo "namespace: $names"
-    names=qa
+for names in $( kubectl get ns | grep -v NAME | awk '{print $1}' ); do
+    echo "namespace: $names"
     for podd in $( kubectl get pod -n $names | grep -v NAME | awk '{print $1}' ); do
         #echo -n "Deployment: $dep "
+        unset value
         value=$( kubectl exec -n $names -it $podd -- env | grep WAIT )
         if [[ ! -z "${value}" ]]; then
+            # these next 3 lines, all they do is to pad variable $podd with spaces so it is 60 chars in lenght. 
             ten="          " 
             sixty="$ten$ten$ten$ten$ten$ten" 
-            #y="very short text"
             podd="${podd:0:60}${sixty:0:$((60 - ${#podd}))}"
             echo "pod: ${podd}; value: ${value}" 
-        else
-            ten="          " 
-            sixty="$ten$ten$ten$ten$ten$ten" 
-            #y="very short text"
-            podd="${podd:0:60}${sixty:0:$((60 - ${#podd}))}"
-            echo "pod: ${podd}; no value found. " 
+        # else
+        #     these next 3 lines, all they do is to pad variable $podd with spaces so it is 60 chars in lenght. 
+        #     ten="          " 
+        #     sixty="$ten$ten$ten$ten$ten$ten" 
+        #     podd="${podd:0:60}${sixty:0:$((60 - ${#podd}))}"
+        #     echo "pod: ${podd}; no value found. " 
         fi
     done
-#done
+done
 echo "Done."
 echo ""
 exit 0
